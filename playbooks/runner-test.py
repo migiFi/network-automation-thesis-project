823275baef3a2f9e12d23cc288c5a1359dd7203d
miggy <miguela@metropolia.fi>
2024-02-13 16:04:27 +0200

    change the file structure, tested the playbooks and python script. Changes need to be made in the startup config and before testing again

--- playbooks/runner-test.py
+++ playbooks/runner-test.py
@@ -3,19 +3,19 @@ import serial
 import time
 
 devices = {
-    'master': '/dev/ttyUSB0',
+    'nodeOne': '/dev/ttyUSB0',
 }
 
 commands = {
-    'master': [
+    'nodeOne': [
         b'enable\n',
         b'confifure terminal\n',
-        b'hostname master\n',
+        b'hostname nodeOne\n',
         b'ip domain-name cisco.com',
         b'crypto key generate rsa\n',
         b'line vty 0 15\n',
-        b'login local\n',
         b'transport input ssh\n',
+        b'login local\n',
         b'exit\n',
         b'username cisco privilege 15 secret cisco123\n',
         b'ip ssh version 2\n',
@@ -25,19 +25,19 @@ commands = {
 }
 
 runner = ansible_runner.run(
-    private_data_dir='/home/miggy/pi-dev/network-automation-thesis-project/network_ansible/', 
-    playbook='rollback-playbook.yml'
+    private_data_dir='/home/miggy/network-automation-thesis-project/network_ansible/', 
+    playbook='testing.yml'
 )
 
 while "Reloading device" not in runner.events:
-    time.sleep(1)
+    time.sleep(3)
 
 for device, port in devices.items():
     print(f"Configuring {device}...")
     connection = serial.Serial(port, 9600)
     for command in commands[device]:
         connection.write(command)
-        time.sleep(4)
+        time.sleep(6)
     print(f"{device} configuration completed.")
     connection.close()
 
