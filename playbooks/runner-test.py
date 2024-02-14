30de54d48cc300d185dd96ac3edb129c27c574fe
miggy <miguela@metropolia.fi>
2024-02-14 14:38:23 +0200

    python script working with bugs, need to be able to delay each config(if possible), testing.yml playbook working, get/setstate playbook for the ippower working

--- playbooks/runner-test.py
+++ playbooks/runner-test.py
@@ -1,49 +1,29 @@
-import ansible_runner
 import serial
 import time
 
-devices = {
-    'nodeOne': '/dev/ttyUSB0',
-}
+def push_config(device_port, config_file):
+    try:
+        with serial.Serial(device_port, baudrate=9600, timeout=1) as ser:
+            ser.write(b"\r\n")
+            time.sleep(1)
+            ser.write(b"enable\n")
+            time.sleep(1)
+            ser.write(b"configure terminal\n")
+            time.sleep(1)
+            
+            with open(config_file, 'r') as file:
+                for line in file:
+                    ser.write((line + '\n').encode())
+                    time.sleep(0.25)  # Wait for the command to be executed
+                    output = ser.read(ser.inWaiting()) 
+                    print(output.decode(), end='')  #
+                    
+            ser.write(b"end\n")
+            time.sleep(1)
+  
+            print(f"Configuration pushed to device on {device_port}.")
+    except Exception as e:
+        print(f"Failed to push configuration to device on {device_port}: {e}")
 
-commands = {
-    'nodeOne': [
-        b'enable\n',
-        b'confifure terminal\n',
-        b'hostname nodeOne\n',
-        b'ip domain-name cisco.com',
-        b'crypto key generate rsa\n',
-        b'line vty 0 15\n',
-        b'transport input ssh\n',
-        b'login local\n',
-        b'exit\n',
-        b'username cisco privilege 15 secret cisco123\n',
-        b'ip ssh version 2\n',
-        b'write memory\n'
-    ],
-    
-}
-
-runner = ansible_runner.run(
-    private_data_dir='/home/miggy/network-automation-thesis-project/network_ansible/', 
-    playbook='testing.yml'
-)
-
-while "Reloading device" not in runner.events:
-    time.sleep(3)
-
-for device, port in devices.items():
-    print(f"Configuring {device}...")
-    connection = serial.Serial(port, 9600)
-    for command in commands[device]:
-        connection.write(command)
-        time.sleep(6)
-    print(f"{device} configuration completed.")
-    connection.close()
-
-while True:
-    output = connection.readline().decode('utf-8')
-    if "SSH connectivity restored" in output:
-        break
-
-runner.wait()
+push_config('/dev/ttyUSB1', '/home/miggy/network-automation-thesis-project/config_files/cisco-router-simple.txt')
+push_config('/dev/ttyUSB0', '/home/miggy/network-automation-thesis-project/config_files/cisco-switch-simple.txt')
