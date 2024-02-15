d879ac67e38542ff76ccffa753d391df178c9656
migiFi <miguela@metropolia.fi>
2024-02-15 23:20:47 +0200

    setState playbook added to the playbook runner script, need to be tested, and changed some file names.

--- playbooks/main_playbook_runner.py
+++ playbooks/main_playbook_runner.py
@@ -1,5 +1,13 @@
 import serial
 import time
+import subprocess
+
+def run_ansible_playbook(playbook_path):
+    try:
+        result = subprocess.run(['ansible-playbook', playbook_path], check=True)
+        print(f"Playbook was executed successfully: {playbook_path}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to execute the playbook {playbook_path}: {e}")
 
 def push_config(device_port, config_file):
     try:
@@ -14,8 +22,8 @@ def push_config(device_port, config_file):
             with open(config_file, 'r') as file:
                 for line in file:
                     ser.write((line + '\n').encode())
-                    time.sleep(0.25)  # Wait for the command to be executed
-                    output = ser.read(ser.inWaiting()) 
+                    time.sleep(0.25) 
+                    output = ser.read(ser.inWaiting())
                     print(output.decode(), end='')  #
                     
             ser.write(b"end\n")
@@ -25,5 +33,14 @@ def push_config(device_port, config_file):
     except Exception as e:
         print(f"Failed to push configuration to device on {device_port}: {e}")
 
+playbook_path =  ' '# need ansible playbook path from master node
+run_ansible_playbook(playbook_path)
+
+print("Waiting for devices to boot up...")
+time.sleep(180) # the time it takes for devices to boot up <<<--- needs a more accurate time 
+
 push_config('/dev/ttyUSB1', '/home/miggy/network-automation-thesis-project/config_files/cisco-router-simple.txt')
+
+time.sleep(8)
+
 push_config('/dev/ttyUSB0', '/home/miggy/network-automation-thesis-project/config_files/cisco-switch-simple.txt')
