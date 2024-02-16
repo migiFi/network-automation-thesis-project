9116090d88561513983ff9448fe4a8d3f7c72532
miggy <miguela@metropolia.fi>
2024-02-16 14:55:05 +0200

    new switch added to the config, main_runner script tested(working), need to add time stamps for scripts and save a back up file or the running config on device before reload

--- playbooks/main_playbook_runner.py
+++ playbooks/main_playbook_runner.py
@@ -12,35 +12,45 @@ def run_ansible_playbook(playbook_path):
 def push_config(device_port, config_file):
     try:
         with serial.Serial(device_port, baudrate=9600, timeout=1) as ser:
+            # skipping the initial configuration dialog
             ser.write(b"\r\n")
-            time.sleep(1)
+            time.sleep(0.75)
+            ser.write(b"no\n")
+            time.sleep(0.75)
+            
+            print("Waiting for device to be ready.")
+            time.sleep(5)
+            
+            ser.write(b"\r\n")
+            time.sleep(0.75)
             ser.write(b"enable\n")
-            time.sleep(1)
+            time.sleep(0.75)
             ser.write(b"configure terminal\n")
-            time.sleep(1)
+            time.sleep(0.75)
             
             with open(config_file, 'r') as file:
                 for line in file:
                     ser.write((line + '\n').encode())
-                    time.sleep(0.25) 
+                    time.sleep(0.60) 
                     output = ser.read(ser.inWaiting())
                     print(output.decode(), end='')  #
                     
             ser.write(b"end\n")
             time.sleep(1)
   
-            print(f"Configuration pushed to device on {device_port}.")
+            print(f"Successfully pushed config to device on {device_port}.")
     except Exception as e:
         print(f"Failed to push configuration to device on {device_port}: {e}")
 
-playbook_path =  ' '# need ansible playbook path from master node
+playbook_path =  '/home/miggy/network-automation-thesis-project/playbooks/ippower_setstate.yml'
 run_ansible_playbook(playbook_path)
 
 print("Waiting for devices to boot up...")
-time.sleep(180) # the time it takes for devices to boot up <<<--- needs a more accurate time 
+time.sleep(160) # boot up time approx. 3 minutes  
 
-push_config('/dev/ttyUSB1', '/home/miggy/network-automation-thesis-project/config_files/cisco-router-simple.txt')
+push_config('/dev/ttyUSB1',
+            '/home/miggy/network-automation-thesis-project/config_files/cisco_switchTwo.txt')
 
-time.sleep(8)
+#time.sleep(8)
 
-push_config('/dev/ttyUSB0', '/home/miggy/network-automation-thesis-project/config_files/cisco-switch-simple.txt')
+#push_config('/dev/ttyUSB0', '/home/miggy/network-automation-thesis-project/config_files/cisco-switch.txt')
--- playbooks/playbook_runner.py
+++ playbooks/playbook_runner.py
@@ -14,7 +14,7 @@ def push_config(device_port, config_file):
             with open(config_file, 'r') as file:
                 for line in file:
                     ser.write((line + '\n').encode())
-                    time.sleep(0.25)  # Wait for the command to be executed
+                    time.sleep(0.75)  # Wait for the command to be executed
                     output = ser.read(ser.inWaiting()) 
                     print(output.decode(), end='')  #
                     
@@ -25,6 +25,6 @@ def push_config(device_port, config_file):
     except Exception as e:
         print(f"Failed to push configuration to device on {device_port}: {e}")
 
-push_config('/dev/ttyUSB1', '/home/miggy/network-automation-thesis-project/config_files/cisco-router-simple.txt')
+push_config('/dev/ttyUSB0', '/home/miggy/network-automation-thesis-project/config_files/cisco_switch_simple.txt')
 time.sleep(7)
-push_config('/dev/ttyUSB0', '/home/miggy/network-automation-thesis-project/config_files/cisco-switch-simple.txt')
+push_config('/dev/ttyUSB1', '/home/miggy/network-automation-thesis-project/config_files/cisco_router_simple.txt')
